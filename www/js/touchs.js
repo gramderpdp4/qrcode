27da80590382977bc7ead1557a5687d36e6b20a0
gramderpdp4 <74129335+gramderpdp4@users.noreply.github.com>
2022-05-29 22:04:00 -0300

    dfdfdf

--- www/js/touchs.js
+++ www/js/touchs.js
@@ -3,28 +3,15 @@ function TouchMove(Event, Element, Navbar, Subnavbar){
 
     const ElementScrollTop = Element.scrollTop;
 
-    let StateScrolled = 0
-    let Scrolled = ElementScrollTop
-
-    if(StateScrolled == 0){
-
-        if(Scrolled < 40){
-
-            Scrolled = ElementScrollTop - 20
-
-        }
-
-        if(Scrolled < 165){
-
-            document.documentElement.style.setProperty('--animation-scrolled', `-${Scrolled}%`);
+        if(ElementScrollTop <= 165 || ElementScrollTop >= 0){
+            document.documentElement.style.setProperty('--animation-scrolled', `-${ElementScrollTop}%`);
 
             if(!Subnavbar.classList.contains("animation-scrolled")){
 
                 Subnavbar.classList.add("animation-scrolled")
                 Navbar.classList.add("animation-scrolled")
 
-            }           
-        }
-    }
+            }          
+        } 
 
 }
